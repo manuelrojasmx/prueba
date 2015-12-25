0ca607f7608d45c2a3007a4181c5826d344be541
manuelrojasmx <manuel.rojas.mejia@me.com>
2015-12-24 21:38:16 -0700

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Manuel Rojas on 12/24/15.
+//  Copyright © 2015 Solemti S.A. de C.V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
